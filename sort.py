a0d97a6b4bcebcd46ada5b7e6c64f0cbda3b2f23
ajp-2000 <109751905+ajp-2000@users.noreply.github.com>
2023-01-23 20:53:18 +0000

    0.0.2
    Added heap sort

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import sys
 
 # Insertion sort: dir = 1 for ascending; dir = -1 for descending
@@ -37,7 +38,6 @@ def merge(nums, p, q, r, dir = 1):
 
     return nums
 
-# Merge sort
 def merge_sort(nums, p = 0, r = sys.maxsize, dir = 1):
     if r == sys.maxsize:
         r = len(nums) - 1
@@ -49,6 +49,99 @@ def merge_sort(nums, p = 0, r = sys.maxsize, dir = 1):
         nums = merge(nums, p, q, r, dir)
     
     return nums
+
+def bubble_sort(nums, dir = 1):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums)-1, i, -1):
+            if nums[j]*dir < nums[j-1]*dir:
+                # Swap nums[j] and nums[j-1]
+                temp = nums[j]
+                nums[j] = nums[j-1]
+                nums[j-1] = temp
+
+    return nums
+
+# Framework for heap sort. We'll build this from scratch as proof of concept
+class heap:
+    size = 0
+    height = 0
+    array = []
+
+    def __init__(self, nums):
+        self.array = nums
+        self.size = len(nums)
+        self.height = math.ceil(math.log2(self.size+1))
+    
+    # Return the *index* of the left child
+    def left(self, index):
+        index += 1
+        index *= 2
+        index -= 1
+        return index
+    
+    def right(self, index):
+        index += 1
+        index *= 2
+        return index
+
+    # Assume the trees at left(index) and right(index) are heapified, but that array[index] might be out of place
+    # dir = 1 for max heapifying; dir = -1 for min heapifying
+    def heapify(self, index, dir):
+        l = self.left(index)
+        r = self.right(index)
+
+        # Find the largest out of l, index, and r
+        largest = index
+        for x in [l, r]:
+            if x < self.size:
+                if self.array[x]*dir > self.array[largest]*dir:
+                    largest = x
+
+        # Do a swap if required
+        if largest != index:
+            temp = self.array[index]
+            self.array[index] = self.array[largest]
+            self.array[largest] = temp
+            self.heapify(largest, dir)
+
+    # Use heapify() to max heapify the whole heap
+    def build_heap(self, dir):
+        self.size = len(self.array)
+        for i in range(int(self.size/2), -1, -1):
+            self.heapify(i, dir)
+
+    # For testing
+    def print_tree(self):
+        n = 0
+        for h in range(self.height):
+            for x in range(2 ** h):
+                print(self.array[n], end = " ")
+
+                n += 1
+                if n == self.size:
+                    print()
+                    return
+            print()
+
+def heap_sort(nums, dir = 1):
+    # Build a max heap out of the unsorted list
+    h = heap(nums)
+    h.build_heap(dir)
+    h.print_tree()
+
+    # Sort
+    for i in range(h.size-1, 1, -1):
+        temp = h.array[0]
+        h.array[0] = h.array[i]
+        h.array[i] = temp
+        h.size -= 1
+        h.print_tree()
+        print()
+        h.heapify(0, dir)
+        h.print_tree()
+        print()
+
+    return h.array
 
 # Use the sorting functions:
 def get_list():
@@ -70,17 +163,29 @@ def main():
 
     str = input("How would you like to sort this list? ")
     while (str != "exit"):
-        if str == "insertion" or str == "insertion ascending":
+        if str in ["insertion", "insertion ascending"]:
             nums = insertion_sort(nums)
             break
         elif str == "insertion descending":
             nums = insertion_sort(nums, -1)
             break
-        elif str == "merge" or str == "merge ascending":
+        elif str in ["merge", "merge ascending"]:
             nums = merge_sort(nums)
             break
         elif str == "merge descending":
             nums = merge_sort(nums, dir = -1)
+            break
+        elif str in ["bubble", "bubble ascending"]:
+            nums = bubble_sort(nums)
+            break
+        elif str == "bubble descending":
+            nums = bubble_sort(nums, -1)
+            break
+        elif str in ["heap", "heap ascending"]:
+            nums = heap_sort(nums)
+            break
+        elif str == "heap descending":
+            nums = heap_sort(nums, -1)
             break
         
         str = input("Option not recognised - try again: ")
